c44de1c184b5e3a5539ea23975ebd77961dfd863
JackBekket <jackatwork@yandex.ru>
2024-01-25 18:49:35 +0400

    changed msg template

--- internal/bot/command/msgTemplates.go
+++ internal/bot/command/msgTemplates.go
@@ -1,8 +1,8 @@
 package command
 
 var msgTemplates = map[string]string{
-	"hello":      "Hey, this bot is OpenAI chatGPT.",
-	"case0":      "Input your openAI API key. It can be created at https://platform.openai.com/account/api-keys  ..in case you are try to access localai node you should have local pwd for it",
+	"hello":      "Hey, this bot is working with local ai node.",
+	"case0":      "input your password",
 	"await":      "Awaiting",
 	"case1":      "Choose model to use. ",
 	"codex_help": "``` # describe your task in comments like this or put your lines of code you need to autocomplete ```",
