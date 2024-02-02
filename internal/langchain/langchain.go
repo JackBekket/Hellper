7554d262d2f5b7defef899bd20e23c5aab0ca75f
JackBekket <jackatwork@yandex.ru>
2024-02-02 15:56:00 +0400

    LAI basic

--- internal/langchain/langchain.go
+++ internal/langchain/langchain.go
@@ -16,23 +16,43 @@ import (
 )
 
 func main()  {
-	ctx := context.Background()
+	//ctx := context.Background()
 	env.Load()
 	//env_data := env.LoadAdminData()
 	token := env.GetAdminToken()
 
+	/*
+	completion,err := GenerateContentOAI(token,"gpt-3.5-turbo","What would be a good company name a company that makes colorful socks? Write at least 10 options")
+	if err != nil {
+		log.Println(err)
+	}
+	*/
+
+	completion, err := GenerateContentLAI(token,"wizard-uncensored-13b", "What would be a good company name a company that makes colorful socks? Write at least 10 options")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(completion.Choices[0].Content)
+}
+
+func GenerateContentOAI(api_token string, model_name string, promt string) (*llms.ContentResponse, error) {
+	ctx := context.Background()
+	token := api_token
+
 	llm, err := openai.New(
 		openai.WithToken(token),
-		openai.WithModel("gpt-3.5-turbo"),
+		openai.WithModel(model_name),
 		//llms.WithOptions()
 		//openai.WithBaseURL("http://localhost:8000"),
 	)
 	if err != nil {
 	  log.Fatal(err)
 	}
+
 	content := []llms.MessageContent{
-		llms.TextParts(schema.ChatMessageTypeSystem, "You are a company branding design wizard."),
-		llms.TextParts(schema.ChatMessageTypeHuman, "What would be a good company name a company that makes colorful socks? Write at least 10 options"),
+		llms.TextParts(schema.ChatMessageTypeSystem, "You are a helpfull assistant who execute whatever task human gives you"),
+		llms.TextParts(schema.ChatMessageTypeHuman, promt),
 	}
 
 	completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
@@ -40,10 +60,45 @@ func main()  {
 		return nil
 	}))
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
+		return nil, err
+	}
+	//fmt.Println(completion)
+	return completion, nil
+}
+
+
+func GenerateContentLAI(api_token string, model_name string, promt string) (*llms.ContentResponse, error) {
+	ctx := context.Background()
+	token := api_token
+
+	llm, err := openai.New(
+		openai.WithToken(token),
+		openai.WithModel(model_name),
+		//llms.WithOptions()
+		openai.WithBaseURL("http://localhost:8000"),
+	)
+	if err != nil {
+	  log.Fatal(err)
+	}
+	//log.Println(llm.)
+
+	content := []llms.MessageContent{
+		llms.TextParts(schema.ChatMessageTypeSystem, "You are a helpfull assistant who execute whatever task human gives you"),
+		llms.TextParts(schema.ChatMessageTypeHuman, promt),
 	}
 
-	fmt.Println(completion)
+	completion, err := llm.GenerateContent(ctx, content, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		fmt.Print(string(chunk))
+		return nil
+	}))
+	if err != nil {
+		//log.Fatal(err)
+		return nil, err
+	}
+	//llms.WithOptions()
+	//fmt.Println(completion)
+	return completion, nil
 }
 
 func TestOAI(api_token string)  {
@@ -74,6 +129,7 @@ func TestOAI(api_token string)  {
 
 	fmt.Println(completion)
 }
+
 
 func GenerateFromSinglePromtLocal(prompt string, model_name string) (string,error) {
 	ctx := context.Background()
