b54d12207fd83f7136317234dc67a5d349ae8d70
JackBekket <jackatwork@yandex.ru>
2024-04-12 17:33:58 +0400

    uncomment ctx

--- lib/langchain/langchain.go
+++ lib/langchain/langchain.go
@@ -28,7 +28,7 @@ import (
 
 // I use it for fast testing
 func main()  {
-	//ctx := context.Background()
+	ctx := context.Background()
 	env.Load()
 	//env_data := env.LoadAdminData()
 	token := env.GetAdminToken()
