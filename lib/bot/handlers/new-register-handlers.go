40cf856a6f42b2eaf8806ecce51538402efd00e6
d-jackalope <di.jackalope@gmail.com>
2025-02-26 16:19:42 -0800

    Created a handlers type that allows passing dependencies into the bot handlers and a constructor for it

---
+++ lib/bot/handlers/new-register-handlers.go
@@ -0,0 +1,11 @@
+package handlers
+
+// structure to hold dependencies of other packages: postgres, cache, llmHandlers
+type handlers struct {
+	/*Pass dependencies here */
+}
+
+// Constructor of the handlers type
+func NewHandlersBot( /*Pass dependencies here */ ) *handlers {
+	return &handlers{}
+}
