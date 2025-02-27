7200b8542da02f310dbb867b9940f6546f6d716b
d-jackalope <di.jackalope@gmail.com>
2025-02-26 22:04:39 -0800

    Implemented the main handlers for the bot

--- lib/bot/handlers/new-register-handlers.go
+++ lib/bot/handlers/new-register-handlers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 )
 
 // structure to hold dependencies of other packages: postgres, cache, llmHandlers
@@ -19,9 +20,41 @@ func NewHandlersBot( /*Pass dependencies here */ ) *handlers {
 // Central function for registering bot handlers. New used commands should be added here
 func (h *handlers) NewRegisterHandlers(ctx context.Context, tgb *bot.Bot) {
 	// Router for tg bot command handlers
-	tgb.RegisterHandler(bot.HandlerTypeMessageText, "", bot.MatchTypePrefix, h.cmdRouter)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/", bot.MatchTypePrefix, h.cmdRouter)
 
 	// Router for text message handlers
-	tgb.RegisterHandler(bot.HandlerTypeMessageText, "", bot.MatchTypePrefix, h.textMessageRouter)
+	tgb.RegisterHandlerMatchFunc(matchTextMessage, h.textMessageRouter)
 
+	// Router for callbacks
+	tgb.RegisterHandlerMatchFunc(matchCallbackQuery, h.callbackRouter)
+
+	tgb.RegisterHandlerMatchFunc(matchPhoto, h.photoHandler)
+
+	tgb.RegisterHandlerMatchFunc(matchVoice, h.voiceHandler)
+
+	tgb.RegisterHandlerMatchFunc(matchTgGroup, h.tgGroupHandler)
+
+}
+
+// Rules for calling the handler
+
+func matchTextMessage(update *models.Update) bool {
+	return update.Message.Text != "" && update.Message.Photo == nil
+}
+
+func matchCallbackQuery(update *models.Update) bool {
+	return update.CallbackQuery != nil
+}
+
+func matchPhoto(update *models.Update) bool {
+	return update.Message.Photo != nil
+}
+
+func matchVoice(update *models.Update) bool {
+	return update.Message.Voice != nil
+}
+
+func matchTgGroup(update *models.Update) bool {
+	// Stub for the registration func
+	return false
 }
