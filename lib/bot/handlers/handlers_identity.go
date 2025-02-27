a343d44cdf4e35c00d17fc6356c04c9e660a524d
d-jackalope <di.jackalope@gmail.com>
2025-02-27 04:20:27 -0800

    Created a handler for restoring a user after a drop

--- lib/bot/handlers/handlers_identity.go
+++ lib/bot/handlers/handlers_identity.go
@@ -45,3 +45,38 @@ func (h *handlers) handleNewUser(ctx context.Context, tgb *bot.Bot, update *mode
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 	}
 }
+
+func (h *handlers) handleRecoverUserAfterDrop(ctx context.Context, tgb *bot.Bot, update *models.Update) {
+	if update.Message == nil {
+		return
+	}
+
+	chatID := update.Message.Chat.ID
+	username := update.Message.From.Username
+
+	log.Info().
+		Int64("chat_id", chatID).
+		Str("username", username).
+		Msg("Restoring a registered user")
+
+	user := database.User{
+		ID:           chatID,
+		Username:     username,
+		DialogStatus: 4,
+		Admin:        false,
+		AiSession: database.AiSession{
+			Base_url: h.ai_endpoint,
+		},
+	}
+
+	apiKey, err := h.db_service.GetToken(chatID, 1)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Msg("Ошибка получения API-ключа")
+		return
+	}
+	user.AiSession.GptKey = apiKey
+
+	h.cache.data[chatID] = user
+
+	h.RenderModelsForRegisteredUser(ctx, tgb, update) //todo
+}
