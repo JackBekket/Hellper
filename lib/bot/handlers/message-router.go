072fc8ce516aacb959be222effcda04f0159598b
d-jackalope <di.jackalope@gmail.com>
2025-02-26 19:55:05 -0800

    Router for text message handlers

---
+++ lib/bot/handlers/message-router.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func (h *handlers) textMessageRouter(ctx context.Context, tgb *bot.Bot, update *models.Update) {
+
+}
