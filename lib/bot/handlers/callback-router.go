f79ca72f10ed8bc71e16bd2b77f3c86053a503aa
d-jackalope <di.jackalope@gmail.com>
2025-02-26 22:05:46 -0800

    Implementation of handlers

---
+++ lib/bot/handlers/callback-router.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func (h *handlers) callbackRouter(ctx context.Context, b *bot.Bot, update *models.Update) {
+
+}
