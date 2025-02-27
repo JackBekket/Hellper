4a7649b986e8694bdfc3680a643eb852eebd2652
d-jackalope <di.jackalope@gmail.com>
2025-02-26 19:05:26 -0800

    Registered tg bot command handlers

--- lib/bot/handlers/new-register-handlers.go
+++ lib/bot/handlers/new-register-handlers.go
@@ -17,6 +17,20 @@ func NewHandlersBot( /*Pass dependencies here */ ) *handlers {
 }
 
 // Central function for registering bot handlers. New used commands should be added here
-func (h *handlers) NewRegisterHandlers(ctx context.Context, bot *bot.Bot) {
+func (h *handlers) NewRegisterHandlers(ctx context.Context, tgb *bot.Bot) {
+
+	// Telegram bot command handlers
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/image", bot.MatchTypeExact, h.cmdImage)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/reload", bot.MatchTypeExact, h.cmdReload)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/clear", bot.MatchTypeExact, h.cmdClear)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/purge", bot.MatchTypeExact, h.cmdPurge)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/drop", bot.MatchTypeExact, h.cmdDrop)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/help", bot.MatchTypeExact, h.cmdHelp)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/search_doc", bot.MatchTypeExact, h.cmdSearchDoc)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/instruct", bot.MatchTypeExact, h.cmdInstruct)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/usage", bot.MatchTypeExact, h.cmdUsage)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/helper", bot.MatchTypeExact, h.cmdHelper)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/setContext", bot.MatchTypeExact, h.cmdSetContext)
+	tgb.RegisterHandler(bot.HandlerTypeMessageText, "/clearContext", bot.MatchTypeExact, h.cmdClearContext)
 
 }
