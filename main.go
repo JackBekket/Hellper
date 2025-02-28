f6135878746b5595671a378abd612c24fba072ba
d-jackalope <di.jackalope@gmail.com>
2025-02-28 01:41:31 -0800

    Changed global logger to zerolog

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
+	"time"
 
 	"github.com/JackBekket/hellper/lib/bot/command"
 	"github.com/JackBekket/hellper/lib/bot/dialog"
 	"github.com/JackBekket/hellper/lib/database"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-
-	_ = godotenv.Load()
-
-	token := os.Getenv("TG_KEY")
-	db_link := os.Getenv("DB_LINK")
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:          os.Stdout,
+		TimeFormat:   time.DateTime,
+		TimeLocation: time.Local,
+	})
 
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Fatalf("tg token missing: %v\n", err)
+		log.Fatal().Err(err).Msg("tg token missing")
 	}
 
-	// in-memory (cash) db. 
+	// in-memory (cash) db.
 	usersDatabase := database.UsersMap
 
-	//
 	db, err := database.NewHandler(db_link)
 	if err != nil {
-		log.Fatalf("failed to create database service: %v", err)
+		log.Fatal().Err(err).Msg("failed to create database service")
 	}
 	db_service, err := database.NewAIService(db)
 
@@ -48,7 +48,7 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	// handling any incoming updates through channel
-	go dialog.HandleUpdates(upd_ch, bot, *comm,db_service)
+	go dialog.HandleUpdates(upd_ch, bot, *comm, db_service)
 
 	for update := range updates {
 		upd_ch <- update
