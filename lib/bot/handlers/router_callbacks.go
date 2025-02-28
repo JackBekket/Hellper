a776aa3c7262a520be44462edd42d7e24516f6fd
d-jackalope <di.jackalope@gmail.com>
2025-02-28 00:24:28 -0800

    Implemented a callback handler that receives an AI model in the response

--- lib/bot/handlers/router_callbacks.go
+++ lib/bot/handlers/router_callbacks.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"context"
+	"strings"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"github.com/rs/zerolog/log"
 )
 
 //todo
@@ -26,5 +28,65 @@ func (h *handlers) callbackRouter(ctx context.Context, tgb *bot.Bot, update *mod
 
 	default: // todo: error msg
 	}
+
+}
+
+// 4 - status_AIModelSelectionChoice. Old func name - HandleModelChoose
+func (h *handlers) handleAIModelSelectionCallback(ctx context.Context, tgb *bot.Bot, update *models.Update) {
+	chatID := update.CallbackQuery.From.ID
+	content := update.CallbackQuery.Data
+
+	messageID := update.CallbackQuery.ID
+	callbackResponse := &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: messageID,
+		Text:            "🐈💨",
+	}
+	_, err := tgb.AnswerCallbackQuery(ctx, callbackResponse)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
+		return
+
+	}
+
+	// Use strings.Split to separate the string by "_".
+	parts := strings.Split(content, "_")
+	if len(parts) < 2 {
+		log.Error().Int64("chat_id", chatID).Str("content", content).Caller().Msg("invalid callback data format")
+		return
+	}
+	aiModelName := parts[1]
+
+	user, ok := h.cache.GetUser(chatID)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Msg("user not found in cache")
+		return
+		// todo: Add actions in case the user is not found in the cache
+	}
+
+	msg := &bot.SendMessageParams{
+		ChatID: chatID,
+		Text:   msg_Session_model + aiModelName,
+	}
+	_, err = tgb.SendMessage(ctx, msg)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+
+	langMsg := &bot.SendMessageParams{
+		ChatID:      chatID,
+		Text:        msg_Choose_lang,
+		ReplyMarkup: renderLangInlineKeyboard(),
+	}
+
+	_, err = tgb.SendMessage(ctx, langMsg)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+	// status_ConnectingToAiWithLang
+	user.DialogStatus = 5
+	user.AiSession.GptModel = aiModelName
+	h.cache.UpdateUser(user)
 
 }
