5362a6eb0ad3a60b21591e1198810646e7773f64
d-jackalope <di.jackalope@gmail.com>
2025-02-28 00:29:54 -0800

    Implemented a handler that sends the first AI prompt and returns a response to the user

--- lib/bot/handlers/router_callbacks.go
+++ lib/bot/handlers/router_callbacks.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"strings"
 
+	"github.com/JackBekket/hellper/lib/database"
+	"github.com/JackBekket/hellper/lib/langchain"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"github.com/rs/zerolog/log"
@@ -125,4 +127,50 @@ func (h *handlers) handleConnectingToAiWithLangCallback(ctx context.Context, tgb
 		Msg("Starting AI conversation")
 
 	go h.handleStartAiConversationWithLang(ctx, tgb, chatID, langPromt)
+}
+
+// old name func - SetupSequenceWithKey
+func (h *handlers) handleStartAiConversationWithLang(ctx context.Context, tgb *bot.Bot, chatID int64, langPromt string) {
+	user, ok := h.cache.GetUser(chatID)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Msg("user not found in cache")
+		return
+		// todo: Add actions in case the user is not found in the cache
+	}
+
+	probe, response, err := langchain.RunNewAgent(user.AiSession.GptKey, user.AiSession.GptModel, h.baseURL, langPromt)
+	if err != nil {
+		msg := &bot.SendMessageParams{ChatID: chatID, Text: msg_AI_client_failure}
+		_, err = tgb.SendMessage(ctx, msg)
+		if err != nil {
+			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		}
+
+		videoMsg, err := getMsgRandomErrorVideo(chatID)
+		_, err = tgb.SendVideo(ctx, videoMsg)
+		if err != nil {
+			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending video message")
+		}
+		log.Warn().Int64("chat_id", chatID).Str("username", user.Username).Msg("The user was removed from the cache due to an authentication issue.")
+		h.cache.DeleteUser(chatID)
+		return
+	}
+
+	msg := &bot.SendMessageParams{ChatID: chatID, Text: response}
+	_, err = tgb.SendMessage(ctx, msg)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+
+	user.DialogStatus = 6
+	user.AiSession.DialogThread = *probe
+
+	// TODO: Replace with a thread-safe one
+	usage := database.GetSessionUsage(user.ID)
+	user.AiSession.Usage = usage
+
+	h.cache.UpdateUser(user)
+	log.Info().Int64("chat_id", chatID).Str("username", user.Username).Str("ai_endpoint", h.baseURL).
+		Msg("AI conversation completed successfully")
 }
