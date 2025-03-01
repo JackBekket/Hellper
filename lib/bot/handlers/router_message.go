9fee88e2851a85ea1ebf9a31a3881e857f6fc1a2
d-jackalope <di.jackalope@gmail.com>
2025-03-01 06:17:43 -0800

    Completed the router; Implemented the main cyclical handler StartDialogSequence

--- lib/bot/handlers/router_message.go
+++ lib/bot/handlers/router_message.go
@@ -4,13 +4,31 @@ import (
 	"context"
 	"strings"
 
+	"github.com/JackBekket/hellper/lib/agent"
+	"github.com/JackBekket/hellper/lib/database"
+	"github.com/JackBekket/hellper/lib/langchain"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"github.com/rs/zerolog/log"
+	"github.com/tmc/langchaingo/llms"
 )
 
 // Router for text message handlers
 func (h *handlers) textMessageRouter(ctx context.Context, tgb *bot.Bot, update *models.Update) {
+	chatID := update.Message.Chat.ID
+	user, ok := h.cache.GetUser(chatID)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Msg("user not found in cache")
+		return
+		// todo: Add actions in case the user is not found in the cache
+	}
+	switch user.DialogStatus {
+	case status_AIModelSelectionKeyboard:
+		h.handleSendAIModelSelectionKeyboard(ctx, tgb, update)
+	case status_StartDialogSequence:
+		go h.handleStartDialogSequence(ctx, tgb, update)
+	default: // todo: error msg
+	}
 
 }
 
@@ -52,4 +70,83 @@ func (h *handlers) handleSendAIModelSelectionKeyboard(ctx context.Context, tgb *
 	if err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 	}
+}
+
+// Dialog_Status 6 -> 6 (loop) - status_StartDialogSequence
+func (h *handlers) handleStartDialogSequence(ctx context.Context, tgb *bot.Bot, update *models.Update) {
+	chatID := update.Message.Chat.ID
+	user, ok := h.cache.GetUser(chatID)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Msg("user not found in cache")
+		return
+		// todo: Add actions in case the user is not found in the cache
+	}
+
+	model := user.AiSession.GptModel
+	prompt := update.Message.Text
+	thread := user.AiSession.DialogThread
+	log.Info().Str("gpt_model", model).Str("prompt", prompt).Msg("processing GPT request")
+
+	post_session, resp, err := langchain.ContinueAgent(user.AiSession.GptKey, model, h.config.AI_endpoint, prompt, &thread)
+	if err != nil {
+		videoMsg, err := getErrorMsgWithRandomVideo(chatID)
+		if err != nil {
+			log.Error().Err(err).Caller().Msg("")
+			return
+		}
+
+		_, err = tgb.SendVideo(ctx, videoMsg)
+		if err != nil {
+			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending video message")
+		}
+		log.Warn().Int64("chat_id", chatID).Str("username", user.Username).Msg("The user was removed from the cache due to an authentication issue.")
+		h.cache.DeleteUser(chatID)
+		return
+	}
+
+	msg := &bot.SendMessageParams{ChatID: chatID, Text: resp, ParseMode: "MARKDOWN"}
+	_, err = tgb.SendMessage(ctx, msg)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+
+	user.DialogStatus = status_StartDialogSequence
+
+	usage, ok := h.cache.GetUsage(chatID)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Msg("usage not found in cache")
+		return
+		// todo: Add actions in case the user is not found in the cache
+	}
+
+	user.AiSession.Usage = usage.Usage
+	user.AiSession.DialogThread = *post_session
+	h.cache.UpdateUser(user)
+
+	totalTurns := len(thread.ConversationBuffer)
+	log.Info().Int("total_turns", totalTurns).Caller().Msg("conversation turns counted")
+
+	// here we save user conversation to the db?
+	// Update the user in the database here. Yes, everything that happens with the "user" should be immediately visible.
+	buffer := post_session.ConversationBuffer
+	last_msg := buffer[len(buffer)-1]
+	humanType := agent.CreateMessageContentHuman(prompt)
+	threadID := chatID
+
+	if err := updateUserHistoryInDB(h.db_service, chatID, threadID, model, humanType[0], last_msg); err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Str("model", model).Caller().Msg("failed to update user history")
+		return
+	}
+
+}
+
+func updateUserHistoryInDB(db *database.Service, chatID, threadID int64, model string, humanType, last_msg llms.MessageContent) error {
+	if err := db.UpdateHistory(chatID, 1, chatID, threadID, model, humanType); err != nil {
+		return err
+	} //endpointID is hardcoded and why chatID is threadID?
+	if err := db.UpdateHistory(chatID, 1, chatID, threadID, model, last_msg); err != nil {
+		return err
+	}
+	return nil
 }
