29b7a5dae8bb208ef16eeeda5a2bcefa833f54ce
d-jackalope <di.jackalope@gmail.com>
2025-02-28 16:10:20 -0800

    This package is designed for centralized storage of configurations required for service operation; AIconfig struct if configuration for working with various AI APIs

---
+++ lib/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+// This package is designed for centralized storage of configurations required for service operation
+
+// Configuration for working with various AI APIs
+type AIConfig struct {
+	AI_endpoint            string
+	BaseURL                string
+	ImageGenerationModel   string
+	ImageGenerationSuffix  string
+	ImageRecognitionModel  string
+	ImageRecognitionSuffix string
+	VoiceRecognitionModel  string
+	VoiceRecognitionSuffix string
+}
