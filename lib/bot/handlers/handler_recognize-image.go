5d2ba3ac89bc2d5b3deb4436c1499ccf087b8fe3
d-jackalope <di.jackalope@gmail.com>
2025-03-01 01:54:28 -0800

    Implemented a handler responsible for image recognition

---
+++ lib/bot/handlers/handler_recognize-image.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/JackBekket/hellper/lib/localai/imageRecognition"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/rs/zerolog/log"
+)
+
+// Image recognition handler
+func (h *handlers) handleRecognizeImage(ctx context.Context, tgb *bot.Bot, update *models.Update) {
+	chatID := update.Message.Chat.ID
+	params := &bot.GetFileParams{FileID: update.Message.Photo[0].FileID}
+
+	msgFailedRecognizeFunc := func() {
+		msg := &bot.SendMessageParams{ChatID: chatID, Text: errMsg_FailedRecognizeImage}
+		_, err := tgb.SendMessage(ctx, msg)
+		if err != nil {
+			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		}
+	}
+	file, err := tgb.GetFile(ctx, params)
+	if err != nil {
+		msgFailedRecognizeFunc()
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("failed to get file from Telegram API")
+		return
+	}
+
+	fileURL := urlTelegramServeFilesConstructor(tgb.Token(), file.FilePath)
+	prompt := update.Message.Caption
+	if update.Message.Caption == "" {
+		prompt = basePrompt_RecognizeImage
+	}
+
+	url := urlConnectionToAIConstructor(h.config.AI_endpoint, h.config.ImageRecognitionSuffix)
+	model := h.config.ImageRecognitionModel
+	recognize, err := imageRecognition.ImageRecognitionLAI(url, model, tgb.Token(), fileURL, prompt)
+	if err != nil {
+		msgFailedRecognizeFunc()
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("failed to get file from Telegram API")
+		return
+	}
+
+	msg := &bot.SendMessageParams{ChatID: chatID, Text: recognize}
+	_, err = tgb.SendMessage(ctx, msg)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+}
