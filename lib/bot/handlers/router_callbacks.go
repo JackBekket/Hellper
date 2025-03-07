db03a14fe2d128668ddb215caad80aa01010a85c
d-jackalope <di.jackalope@gmail.com>
2025-03-07 15:19:39 -0800

    Minor improvements. The default case in the callback router now handles all unknown callbacks.  New handlers have been added to process the user's selection — AI service (localai and openai). The callback sends the authentication method. In the case of OpenAI, the user is immediately prompted to enter the API token. In the case of localai, the user is asked to select a server (federated, testing, weights) and then enter the localai token.

--- lib/bot/handlers/router_callbacks.go
+++ lib/bot/handlers/router_callbacks.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/JackBekket/hellper/lib/database"
@@ -13,58 +14,164 @@ import (
 )
 
 func (h *handlers) callbackRouter(ctx context.Context, tgb *bot.Bot, update *models.Update) {
-	chatID := update.CallbackQuery.From.ID
+	callback := update.CallbackQuery
+	chatID := callback.From.ID
 	user, ok := ctx.Value(database.UserCtxKey).(database.User)
 	if !ok {
 		log.Error().Int64("chat_id", chatID).Caller().Msg("user not found in context")
 		return
 	}
 
-	fmt.Println("ПОЛУЧЕН КОЛБЕК")
-
 	switch user.DialogStatus {
-	case statusAIModelSelectionChoice:
-		h.handleAIModelSelectionCallback(ctx, tgb, update)
-	case statusConnectingToAiWithLang:
+	case statusAIModelSelectionChoiceCallback:
+		h.handleAIModelSelectionCallback(ctx, tgb, callback)
+	case statusConnectingToAiWithLangCallback:
+		// Can also accept a text message with the first prompt
 		h.handleConnectingToAiWithLangCallback(ctx, tgb, update)
-	default: // todo: error msg
-
-		// проверка языка
-
-		messageID := update.CallbackQuery.ID
-		callbackResponse := &bot.AnswerCallbackQueryParams{
-			CallbackQueryID: messageID,
-			Text:            "Неизвестный Хендлер",
-		}
-		_, err := tgb.AnswerCallbackQuery(ctx, callbackResponse)
-		if err != nil {
+	case statusAuthMethodCallback:
+		h.handleAuthMethodCallback(ctx, tgb, callback)
+	case statusLocalAIProviderCallback:
+		h.handleLocalAIProviderCallback(ctx, tgb, callback)
+	default:
+		if _, err := tgb.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+			CallbackQueryID: callback.ID,
+			Text:            "🐈💨",
+		}); err != nil {
 			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
 			return
-
 		}
-
-		if user.AiSession.LocalAIToken == "" {
+		if user.AiSession.AIToken == "" {
 			h.handleNewUserRegistration(ctx, tgb, update)
 		}
-
-		h.handleConnectingToAiWithLangCallback(ctx, tgb, update)
 
 	}
 
 }
 
-// 4 - status_AIModelSelectionChoice. Old func name - HandleModelChoose
-func (h *handlers) handleAIModelSelectionCallback(ctx context.Context, tgb *bot.Bot, update *models.Update) {
-	chatID := update.CallbackQuery.From.ID
-	content := update.CallbackQuery.Data
+func (h *handlers) handleAuthMethodCallback(ctx context.Context, tgb *bot.Bot, callback *models.CallbackQuery) {
+	chatID := callback.From.ID
+	data := callback.Data
 
-	messageID := update.CallbackQuery.ID
-	callbackResponse := &bot.AnswerCallbackQueryParams{
-		CallbackQueryID: messageID,
+	if _, err := tgb.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: callback.ID,
 		Text:            "🐈💨",
+	}); err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
+		return
 	}
-	_, err := tgb.AnswerCallbackQuery(ctx, callbackResponse)
+	user, ok := ctx.Value(database.UserCtxKey).(database.User)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Caller().Msg("user not found in context")
+		return
+	}
+
+	authMethod, err := strconv.Atoi(data)
 	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("сonversion error")
+		return
+	}
+	aiProviders, err := h.dbService.GetAIProvidersName(int64(authMethod))
+	if err != nil || len(aiProviders) == 0 {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error retrieving providers name from the database")
+		return
+	}
+	if authMethod == database.AuthMethodOpenAI {
+		// OpenAI has a single base URL for the API
+		h.handleOpenAIAuthMethod(ctx, tgb, chatID, aiProviders[0])
+		return
+	}
+	//Currently, only localAI has multiple base URLs
+	if _, err := tgb.EditMessageReplyMarkup(ctx, &bot.EditMessageReplyMarkupParams{
+		ChatID:      chatID,
+		MessageID:   callback.Message.Message.ID,
+		ReplyMarkup: renderLocalAIProvidersInlineKeyboard(aiProviders),
+	}); err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error editing replyMarkup")
+		return
+	}
+
+	user.AiSession.AuthMethod = database.AuthMethodLocalAI
+	user.DialogStatus = statusLocalAIProviderCallback
+	h.cache.UpdateUser(user)
+
+}
+
+func (h *handlers) handleOpenAIAuthMethod(ctx context.Context, tgb *bot.Bot, chatID int64, providerName string) {
+	user, ok := ctx.Value(database.UserCtxKey).(database.User)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Caller().Msg("user not found in context")
+		return
+	}
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: chatID,
+		Text:   fmt.Sprintf(msgEnterAPIToken, providerName),
+	}); err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+	provider, err := h.dbService.GetAIProvider(providerName)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error retrieving provider from the database")
+		return
+	}
+	user.DialogStatus = statusAPIToken
+	user.AiSession.ProviderID = provider.ID
+	user.AiSession.BaseURL = provider.BaseURL
+	user.AiSession.ProviderName = provider.Name
+	user.AiSession.AuthMethod = database.AuthMethodOpenAI
+
+	h.cache.UpdateUser(user)
+}
+
+func (h *handlers) handleLocalAIProviderCallback(ctx context.Context, tgb *bot.Bot, callback *models.CallbackQuery) {
+	chatID := callback.From.ID
+	providerName := callback.Data
+
+	if _, err := tgb.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: callback.ID,
+		Text:            "🐈💨",
+	}); err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
+		return
+	}
+
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: chatID,
+		Text:   fmt.Sprintf(msgEnterAPIToken, providerName),
+	}); err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
+		return
+	}
+
+	provider, err := h.dbService.GetAIProvider(providerName)
+	if err != nil {
+		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error retrieving provider from the database")
+		return
+	}
+
+	user, ok := ctx.Value(database.UserCtxKey).(database.User)
+	if !ok {
+		log.Error().Int64("chat_id", chatID).Caller().Msg("user not found in context")
+		return
+	}
+
+	user.DialogStatus = statusAPIToken
+	user.AiSession.ProviderID = provider.ID
+	user.AiSession.BaseURL = provider.BaseURL
+	user.AiSession.ProviderName = provider.Name
+	h.cache.UpdateUser(user)
+
+}
+
+// 4 - status_AIModelSelectionChoice. Old func name - HandleModelChoose
+func (h *handlers) handleAIModelSelectionCallback(ctx context.Context, tgb *bot.Bot, callback *models.CallbackQuery) {
+	chatID := callback.From.ID
+	content := callback.Data
+
+	if _, err := tgb.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: callback.ID,
+		Text:            "🐈💨",
+	}); err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
 		return
 
@@ -78,24 +185,19 @@ func (h *handlers) handleAIModelSelectionCallback(ctx context.Context, tgb *bot.
 	}
 
 	aiModelName := parts[1]
-	msg := &bot.SendMessageParams{
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: chatID,
-		Text:   msgSessionModel + aiModelName,
-	}
-	_, err = tgb.SendMessage(ctx, msg)
-	if err != nil {
+		Text:   fmt.Sprintf(msgSessionModelFormat, aiModelName),
+	}); err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 		return
 	}
 
-	langMsg := &bot.SendMessageParams{
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID:      chatID,
 		Text:        msgChooseLang,
 		ReplyMarkup: renderLangInlineKeyboard(),
-	}
-
-	_, err = tgb.SendMessage(ctx, langMsg)
-	if err != nil {
+	}); err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 		return
 	}
@@ -106,44 +208,38 @@ func (h *handlers) handleAIModelSelectionCallback(ctx context.Context, tgb *bot.
 		log.Error().Int64("chat_id", chatID).Caller().Msg("user not found in context")
 		return
 	}
-	user.DialogStatus = statusConnectingToAiWithLang
+	user.DialogStatus = statusConnectingToAiWithLangCallback
 	user.AiSession.GptModel = aiModelName
 	h.cache.UpdateUser(user)
 
 }
 
 func (h *handlers) handleConnectingToAiWithLangCallback(ctx context.Context, tgb *bot.Bot, update *models.Update) {
-	chatID := update.CallbackQuery.From.ID
-	lang := update.CallbackQuery.Data
-
-	log.Info().Int64("chat_id", chatID).Str("lang", lang).Msg("User initiated AI connection via callback")
-
-	messageID := update.CallbackQuery.ID
-	callbackResponse := &bot.AnswerCallbackQueryParams{
-		CallbackQueryID: messageID,
-		Text:            "🐈💨",
+	var lang, langPrompt string
+	var chatID int64
+	if update.Message == nil {
+		chatID = update.CallbackQuery.From.ID
+		lang = update.CallbackQuery.Data
+		log.Info().Int64("chat_id", chatID).Str("lang", lang).Msg("User initiated AI connection via callback")
+		if _, err := tgb.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+			CallbackQueryID: update.CallbackQuery.ID,
+			Text:            "🐈💨",
+		}); err != nil {
+			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
+			return
+		}
+		langPrompt = getInitialLangPrompt(lang)
+	} else {
+		langPrompt = update.Message.Text
 	}
-	_, err := tgb.AnswerCallbackQuery(ctx, callbackResponse)
-	if err != nil {
-		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error answering callback query")
-		return
-	}
 
-	msg := &bot.SendMessageParams{
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: chatID,
 		Text:   msgConnectingAINode,
-	}
-	_, err = tgb.SendMessage(ctx, msg)
-	if err != nil {
+	}); err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 		return
 	}
-
-	// I commented out the line because the context with the value is not used anywhere
-	//ctxWithValue := context.WithValue(ctx, "user", user)
-	langPrompt := getInitialLangPrompt(lang)
-	log.Info().Int64("chat_id", chatID).Str("language", lang).Str("endpoint", h.config.BaseURL).
-		Msg("Starting AI conversation")
 
 	go h.handleStartAiConversationWithLang(ctx, tgb, chatID, langPrompt)
 }
@@ -155,17 +251,18 @@ func (h *handlers) handleStartAiConversationWithLang(ctx context.Context, tgb *b
 		log.Error().Int64("chat_id", chatID).Caller().Msg("user not found in context")
 		return
 	}
+	log.Info().Int64("chat_id", chatID).Str("endpoint", user.AiSession.BaseURL).
+		Msg("Starting AI conversation")
 
 	model := user.AiSession.GptModel
-	probe, response, err := langchain.RunNewAgent(user.AiSession.LocalAIToken, model, h.config.BaseURL, langPrompt)
+	probe, response, err := langchain.RunNewAgent(user.AiSession.AIToken, model, user.AiSession.BaseURL, langPrompt)
 	if err != nil {
 		videoMsg, err := getErrorMsgWithRandomVideo(chatID)
 		if err != nil {
-			log.Error().Err(err).Caller().Msg("")
+			log.Error().Err(err).Caller().Msg("error generating video message")
 			return
 		}
-		_, err = tgb.SendVideo(ctx, videoMsg)
-		if err != nil {
+		if _, err := tgb.SendVideo(ctx, videoMsg); err != nil {
 			log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending video message")
 		}
 		log.Warn().Int64("chat_id", chatID).Str("username", user.Username).Msg("The user was removed from the cache due to an authentication issue.")
@@ -173,9 +270,7 @@ func (h *handlers) handleStartAiConversationWithLang(ctx context.Context, tgb *b
 		return
 	}
 
-	msg := &bot.SendMessageParams{ChatID: chatID, Text: response}
-	_, err = tgb.SendMessage(ctx, msg)
-	if err != nil {
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{ChatID: chatID, Text: response}); err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 		return
 	}
@@ -187,19 +282,18 @@ func (h *handlers) handleStartAiConversationWithLang(ctx context.Context, tgb *b
 	usage := database.GetSessionUsage(user.ID)
 	user.AiSession.Usage = usage
 
-	h.dbService.CreateLSession(chatID, model, 1)
-
+	h.dbService.CreateAISession(chatID, user.AiSession.GptModel, user.AiSession.ProviderID)
 	h.cache.UpdateUser(user)
-	log.Info().Int64("chat_id", chatID).Str("username", user.Username).Str("BaseURL", h.config.BaseURL).
+	log.Info().Int64("chat_id", chatID).Str("username", user.Username).Str("BaseURL", user.AiSession.BaseURL).
 		Msg("AI conversation completed successfully")
 }
 
 // Returns the initial prompt with the selected language
 func getInitialLangPrompt(lang string) string {
 	switch lang {
-	case "English":
+	case langEnglish:
 		return basePromptLangEN
-	case "Russian":
+	case langRussian:
 		return basePromptLangRU
 	default:
 		return basePromptLangEN
