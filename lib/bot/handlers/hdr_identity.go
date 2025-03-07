79e69a506ff144cdd446ea3465a526de66ac4e83
d-jackalope <di.jackalope@gmail.com>
2025-03-07 15:25:39 -0800

    Minor fixes, aiSession now returns more information, which is now restored in the cache. The recoverUserAfterDrop function has been commented out, and it needs to be completely reimplemented.

--- lib/bot/handlers/hdr_identity.go
+++ lib/bot/handlers/hdr_identity.go
@@ -16,37 +16,31 @@ func (h *handlers) handleNewUserRegistration(ctx context.Context, tgb *bot.Bot, 
 	if update.Message == nil {
 		return
 	}
-
 	chatID := update.Message.Chat.ID
-
-	user := database.User{
+	h.cache.SetUser(chatID, database.User{
 		ID:           chatID,
 		Username:     update.Message.From.Username,
-		DialogStatus: statusAIModelSelectionKeyboardForNewUser,
+		DialogStatus: statusAuthMethodCallback,
 		Admin:        false,
-	}
+	})
 
-	h.cache.SetUser(chatID, user)
-
-	msg := &bot.SendMessageParams{
-		ChatID: chatID,
-		Text:   msgHello,
-	}
-	// послать клавиатуру
-	_, err := tgb.SendMessage(ctx, msg)
-	if err != nil {
+	if _, err := tgb.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      chatID,
+		Text:        msgHello,
+		ReplyMarkup: renderAIServicesInlineKeyboard(),
+	}); err != nil {
 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error sending message")
 	}
 }
 
 // download user data from database into cashe
 func restoreUserSessionFromDB(ds *database.Service, chatID int64, username string) (database.User, error) {
-	aiSession, err := ds.GetSession(chatID)
+	aiSession, err := ds.GetAISession(chatID)
 	if err != nil {
 		return database.User{}, fmt.Errorf("failed to retrieve session: %w", err)
 	}
 
-	LocalAIToken, err := ds.GetToken(chatID, 1)
+	aiToken, err := ds.GetToken(chatID, aiSession.AIProvider.AuthMethod)
 	if err != nil {
 		return database.User{}, fmt.Errorf("error retrieving user's API key : %w", err)
 	}
@@ -56,46 +50,44 @@ func restoreUserSessionFromDB(ds *database.Service, chatID int64, username strin
 		return database.User{}, fmt.Errorf("error loading user's history : %w", err)
 	}
 
-	user := database.User{
+	return database.User{
 		ID:           chatID,
 		Username:     username,
 		DialogStatus: statusStartDialogSequence,
 		Admin:        false,
 		AiSession: database.AiSession{
 			GptModel:     aiSession.Model,
-			Base_url:     aiSession.AIProvider.BaseURL,
-			LocalAIToken: LocalAIToken,
+			BaseURL:      aiSession.AIProvider.BaseURL,
+			AIToken:      aiToken,
+			AuthMethod:   aiSession.AIProvider.AuthMethod,
+			ProviderID:   aiSession.AIProvider.ID,
+			ProviderName: aiSession.AIProvider.Name,
 			DialogThread: database.ChatSessionGraph{
 				ConversationBuffer: history,
 			},
 		},
-	}
-	return user, nil
-
+	}, nil
 }
-func recoverUserAfterDrop(ds *database.Service, chatID int64, username string, BaseURL string) (database.User, error) {
 
-	log.Info().
-		Int64("chat_id", chatID).
-		Str("username", username).
-		Msg("Restoring a registered user")
+// func recoverUserAfterDrop(ds *database.Service, chatID int64, username string) (database.User, error) {
+// 	log.Info().Int64("chat_id", chatID).Str("username", username).Msg("Restoring a registered user")
 
-	LocalAIToken, err := ds.GetToken(chatID, 1)
-	if err != nil {
-		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error retrieving GPT KEY")
-		log.Warn().Int64("chat_id", chatID).Msg("User redirected to registration handler due to GPT KEY error")
-		return database.User{}, err
-	}
-	user := database.User{
-		ID:           chatID,
-		Username:     username,
-		DialogStatus: statusAIModelSelectionChoice,
-		Admin:        false,
-		AiSession: database.AiSession{
-			Base_url:     BaseURL,
-			LocalAIToken: LocalAIToken,
-		},
-	}
+// 	auth, err := ds.GetUserTokensWithAuthMethods(chatID)
+// 	if err != nil {
+// 		log.Error().Err(err).Int64("chat_id", chatID).Caller().Msg("error retrieving GPT KEY")
+// 		log.Warn().Int64("chat_id", chatID).Msg("User redirected to registration handler due to GPT KEY error")
+// 		return database.User{}, err
+// 	}
 
-	return user, nil
-}
+// 	if len(auth) > 1 {
+
+// 	}
+
+// 	return database.User{
+// 		ID:           chatID,
+// 		Username:     username,
+// 		DialogStatus: statusAuthMethodCallback,
+// 		Admin:        false,
+// 		AiSession:    database.AiSession{},
+// 	}, nil
+// }
